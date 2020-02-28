e5a170e00af46e9902f5454a9c4cbaeacf623699
lishuo <lishuocoder@163.com>
2020-02-28 19:25:38 +0800

    增加鼠标放上去显示详细信息

--- src/components/map.vue
+++ src/components/map.vue
@@ -68,7 +68,16 @@
 			},
 			itemHeight: 10,
 			itemWidth: 20
-		}]
+		}],
+		tooltip:{
+			
+			// position: function (pos, params, dom, rect, size) {
+			//       // 鼠标在左侧时 tooltip 显示到右侧，鼠标在右侧时 tooltip 显示到左侧。
+			//       var obj = {top: 60};
+			//       obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 5;
+			//       return obj;
+			//   }
+		}
 	};
 	export default {
 		name: 'Map',
